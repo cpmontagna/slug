1a0ca18ff6c86d39edeb4ce2f62fcf6bd93d9692
Chiara Montagna <chiara.montagna@ingv.it>
2022-03-09 16:35:18 +0100

    sim2

---
+++ stromboli/sim2/README.md
@@ -0,0 +1,3 @@
+# sim2
+Conduit is 170 m long, 0.5 m wide. At time 0, continuous gas injection starts from the bottom of the
+conduit ([movie](postprocessing/movies/movie_inlet.ogv)).
---
+++ stromboli/sim2/postprocessing/pressure/programs/extractPressureTimeSeries.m
@@ -0,0 +1,44 @@
+% extract pressure timeseries on a vertical boundary
+% data is stored as function of space 
+
+% Chiara P. Montagna, INGV Pisa, Feb 2022
+% chiara.montagna@ingv.it
+
+% INPUT one file per time sample, containing point coordinates and pressure
+
+% OUTPUT boundaryPressures.mat, containing coordinates (zetas) and corresponding pressure time series (sampleTimes, pressure)
+
+clear all; 
+close all;
+fclose('all');
+
+% ================================ TO BE SET ==============================
+% sampled times
+sampleTimes = [0:0.1:15.7];
+
+% pressure data path 
+% data stored as loadPath/sampleTime/fileName
+loadPath = '../data/';
+fileName = 'boundaryLine_p.xy';
+
+% save data
+savePath = '../data/';
+
+% ========================================================================
+
+% vertical coordinates
+timeZero = load([loadPath '0/' fileName]);
+zetas = timeZero(:,1);
+
+% load space data and convert to time series
+j = 1; 
+for sampleTimeNow = sampleTimes(1:end)
+    data = load([loadPath num2str(sampleTimeNow) '/' fileName]);
+
+    for i = 1:length(zetas)
+        pressure(i,j) = data(i,2);
+    end
+    j = j+1;
+end
+
+save([savePath 'boundaryPressures.mat'],'sampleTimes','pressure','zetas');
